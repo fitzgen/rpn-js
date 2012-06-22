15ec3250f69a20be520315c459b38fed8d7376ad
Nick Fitzgerald <fitzgen@gmail.com>
2012-06-22 04:05:46 -0700

    Making a proper script to conduct the compilation

---
+++ bin/rpn.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+var rpn = require("rpn");
+
+process.argv.slice(2).forEach(function (file) {
+    var input = fs.readFileSync(file);
+    var output = rpn.compile(input, {
+        originalFilename: file
+    }).toStringWithSourceMap({
+        file: file.replace(/\.[\w]+$/, ".js.map")
+    })
+    fs.writeFileSync(file.replace(/\.[\w]+$/, ".js"), output.code);
+    fs.writeFileSync(file.replace(/\.[\w]+$/, ".js.map"), output.code);
+});
