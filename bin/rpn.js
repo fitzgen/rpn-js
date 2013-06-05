f5f71f68041fe9308f2b799aa365f3ad32761e7b
joseanpg <joseanpg@gmail.com>
2013-06-05 16:24:20 +0300

    Update rpn.js

--- bin/rpn.js
+++ bin/rpn.js
@@ -2,15 +2,16 @@
 var fs = require("fs");
 var rpn = require("rpn");
 
-process.argv.slice(2).forEach(function (file) {
-    var input = fs.readFileSync(file);
-    var output = rpn.compile(input, {
-        originalFilename: file
-    }).toStringWithSourceMap({
-        file: file.replace(/\.[\w]+$/, ".js.map")
-    });
-    var sourceMapFile = file.replace(/\.[\w]+$/, ".js.map");
-    fs.writeFileSync(file.replace(/\.[\w]+$/, ".js"),
-                     output.code + "\n//@ sourceMappingURL=" + sourceMapFile);
-    fs.writeFileSync(sourceMapFile, output.map);
+process.argv.slice(2).forEach(function (sourceFilename) {
+    
+    var codeFilename   = sourceFilename.replace(/\.[\w]+$/, ".js");   
+    var mapFilename    = codeFilename + ".map");    
+    
+    var input          = fs.readFileSync(sourceFilename);
+    var rootSourceNode = rpn.compile(input, {originalFilename: sourceFilename});
+    var output         = rootSourceNode.toStringWithSourceMap({ file: mapFilename});
+
+    fs.writeFileSync(codeFilename, output.code + "\n//@ sourceMappingURL=" + mapFilename);
+    fs.writeFileSync(mapFilename,  output.map);
+    
 });
